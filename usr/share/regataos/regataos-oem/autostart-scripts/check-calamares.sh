6978adb40d09f8c24ca6c2b9a3dd5c8fed0fbc3b
Regata OS Project <regataosproject@gmail.com>
2022-06-22 10:01:04 -0300

    Fix check-calamares service.

--- usr/share/regataos/regataos-oem/autostart-scripts/check-calamares.sh
+++ usr/share/regataos/regataos-oem/autostart-scripts/check-calamares.sh
@@ -5,52 +5,54 @@ cd /
 while :
 do
 
+# Check if the system has already been installed.
+if [[ $(rpm -q calamares) != *"x86"* ]]; then
+	rm -f "/usr/share/regataos/check-calamares.sh"
+	rm -f "/home/visitante/.config/autostart-scripts/check-calamares.sh"
+	break
+fi
+
 # Prepare KDE Plasma for post-installation setup.
-mkdir -p "/home/visitante/.config"
-mkdir -p "/home/visitante/.config/autostart"
-mkdir -p "/home/visitante/.config/autostart-scripts"
+if [[ $(rpm -q regataos-oem) != *"x86"* ]]; then
+	mkdir -p "/home/visitante/.config"
+	mkdir -p "/home/visitante/.config/autostart"
+	mkdir -p "/home/visitante/.config/autostart-scripts"
 
-cp -f "/usr/share/regataos/regataos-oem/autostart/live-installer.desktop" "/home/visitante/.config/autostart/live-installer.desktop"
-cp -f "/usr/share/regataos/regataos-oem/autostart-scripts/check-calamares.sh" "/home/visitante/.config/autostart-scripts/check-calamares.sh"
+	cp -f "/usr/share/regataos/regataos-oem/autostart/live-installer.desktop" "/home/visitante/.config/autostart/live-installer.desktop"
+	cp -f "/usr/share/regataos/regataos-oem/autostart-scripts/check-calamares.sh" "/home/visitante/.config/autostart-scripts/check-calamares.sh"
 
-rm -f "/home/visitante/.config/plasma-org.kde.plasma.desktop-appletsrc"
-cp -f "/usr/share/regataos/regataos-oem/plasma-org.kde.plasma.desktop-appletsrc" "/home/visitante/.config/plasma-org.kde.plasma.desktop-appletsrc"
+	rm -f "/home/visitante/.config/plasma-org.kde.plasma.desktop-appletsrc"
+	cp -f "/usr/share/regataos/regataos-oem/plasma-org.kde.plasma.desktop-appletsrc" "/home/visitante/.config/plasma-org.kde.plasma.desktop-appletsrc"
 
-rm -f "/home/visitante/.config/kdeglobals"
-cp -f "/etc/xdg/kdeglobals" "/home/visitante/.config/kdeglobals"
+	rm -f "/home/visitante/.config/kdeglobals"
+	cp -f "/etc/xdg/kdeglobals" "/home/visitante/.config/kdeglobals"
 
-# Check desktop
-user=$(users | awk '{print $1}')
-test -f "${XDG_CONFIG_HOME:-/home/$user/.config}/user-dirs.dirs" && source "${XDG_CONFIG_HOME:-/home/$user/.config}/user-dirs.dirs"
-desktop_dir="${XDG_DESKTOP_DIR:-/home/$user/Desktop}"
-desktop_folder="$(echo $desktop_dir | cut -d/ -f 3-)"
-desktop_dir="/home/$desktop_folder"
-echo "Desktop: $desktop_dir"
+	# Check desktop
+	user=$(users | awk '{print $1}')
+	test -f "${XDG_CONFIG_HOME:-/home/$user/.config}/user-dirs.dirs" && source "${XDG_CONFIG_HOME:-/home/$user/.config}/user-dirs.dirs"
+	desktop_dir="${XDG_DESKTOP_DIR:-/home/$user/Desktop}"
+	desktop_folder="$(echo $desktop_dir | cut -d/ -f 3-)"
+	desktop_dir="/home/$desktop_folder"
+	echo "Desktop: $desktop_dir"
 
-if test -e "$desktop_dir/trash.desktop"; then
-	cd "$desktop_dir/"
-	rm -rf *.desktop
-	xdotool key F5
-
-elif test -e "$desktop_dir/Home.desktop"; then
-	cd "$desktop_dir/"
-	rm -rf *.desktop
-	xdotool key F5
-
-else
-	if test -e "$desktop_dir/live-installer.desktop"; then
+	if test -e "$desktop_dir/trash.desktop"; then
 		cd "$desktop_dir/"
 		rm -rf *.desktop
 		xdotool key F5
+
+	elif test -e "$desktop_dir/Home.desktop"; then
+		cd "$desktop_dir/"
+		rm -rf *.desktop
+		xdotool key F5
+
+	else
+		if test -e "$desktop_dir/live-installer.desktop"; then
+			cd "$desktop_dir/"
+			rm -rf *.desktop
+			xdotool key F5
+		fi
 	fi
-fi
 
-if [[ $(rpm -q calamares) != *"x86"* ]]; then
-	rm -f "/usr/share/regataos/check-calamares.sh"
-fi
-
-if [[ $(rpm -q regataos-oem) != *"x86"* ]]; then
-	user=$(users | awk '{print $1}')
 	if [[ $user == *"visitante"* ]]; then
 		ps -C calamares > /dev/null
 		if [ $? = 0 ]
@@ -62,8 +64,6 @@ if [[ $(rpm -q regataos-oem) != *"x86"* ]]; then
 	else
 		break
 	fi
-else
-	break
 fi
 
    sleep 1
